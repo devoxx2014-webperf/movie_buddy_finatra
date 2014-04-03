0f2441c134602121949ee650c414ad2d72a86154
k33g <ph.charriere@gmail.com>
2014-04-03 14:58:00 +0200

    ...

--- src/main/scala/org/k33g/MovieBuddy/Preco.scala
+++ src/main/scala/org/k33g/MovieBuddy/Preco.scala
@@ -14,6 +14,18 @@ class Preco {
 
   def distance(preferences: Map[Int, Map[Int, Int]], user1: Int, user2: Int ) : Double = {
 
+    val user1Preferences = preferences(user1)
+    val user2Preferences = preferences(user2)
+
+    println(user1Preferences + " " + user2Preferences)
+
+    val sharedPreferences = user1Preferences.keys.toList intersect user2Preferences.keys.toList
+
+    println(sharedPreferences)
+
+    //java.lang.Double cannot be cast to java.lang.Integer
+    sharedPreferences.foreach( pref => println(pref))
+
     return 0d
 
   }
